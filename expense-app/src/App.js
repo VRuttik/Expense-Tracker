edbb11a6605a91b676233d6fc3e616947d465bb5
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2024-06-20 09:28:36 +0000

    main

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -35,8 +36,8 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started</h2>
 
+      <NewExpense />
       <Expenses item={expenses}/>
     </div>
   );
--- expense-app/src/components/Expenses/ExpenseItem.js
+++ expense-app/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -10,9 +11,10 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{ props.title }</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            
         </Card>
     );
 }
